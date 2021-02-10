5626e98465610d8ae2c48b83b8f38827b0f82e38
ckrivickas <chasekrivickas@Chases-MBP.fios-router.home>
2021-02-10 14:17:30 -0500

    connected api and register survey

--- api/app.py
+++ api/app.py
@@ -2,19 +2,23 @@ from flask import Flask
 from flask_bcrypt import Bcrypt
 from flask_jwt_extended import JWTManager
 from flask_restful import Api
+from flask_cors import CORS
 
 from database.db import initialize_db
 from resources.routes import initialize_routes
 
 # instructions:
 # `pipenv shell`
-# `pipenv install flask flask-bcrypt flask-jwt-extended flask-restful`
+# `pipenv install flask flask-bcrypt flask-jwt-extended flask-restful flask-cors`
 # to set env file location on mac:
 #   `export ENV_FILE_LOCATION=./.env`
 
 
 app = Flask(__name__)
-app.config.from_envvar('ENV_FILE_LOCATION')
+# app.config.from_envvar('ENV_FILE_LOCATION')
+app.config.from_pyfile('config.py', silent=True)
+
+CORS(app)
 
 api = Api(app)
 bcrypt = Bcrypt(app)
--- src/Contact.jsx
+++ src/Contact.jsx
@@ -1,4 +1,5 @@
 import React from 'react';
+import axios from 'axios';
 
 class Contact extends React.Component {
   state = {
@@ -40,11 +41,18 @@ class Contact extends React.Component {
   }
 
     const userSurveyObject = {
-      username: this.state.username,
+      email: this.state.username,
       password: this.state.password,
-      favoriteQuote: favoriteQuote,
+      savedQuotes: [favoriteQuote],
     }
-  console.log(userSurveyObject);
+
+    axios.post(`http://localhost:5000/api/auth/signup`, userSurveyObject )
+      .then(res => {
+        console.log(res);
+        console.log(res.data);
+      })
+
+      // console.log(userSurveyObject);
   };
 
   handleUsernameChange = e => {
@@ -66,14 +74,14 @@ class Contact extends React.Component {
     return (
       <form onSubmit={this.handleSubmit}>
         
-        Username:
+        Email:
         <br />
         <input
           type="text"
           name="username"
           value={this.state.username}
           onChange={this.handleUsernameChange}
-          placeholder="example: plato :)"
+          placeholder="example@gmail.com"
         />
         <br />
 
