e385f4589098c9a539733d462f6a51f3a0048423
Rohan Robinson <rohan.c.robinson.21@dartmouth.edu>
2021-02-24 17:49:28 -0500

    philosophy info rendered from backend

--- src/Explore.js
+++ src/Explore.js
@@ -2,16 +2,44 @@ import React from 'react';
 import Philosophy from './Philosophy';
 import { Link, withRouter } from "react-router-dom";
 import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
+import axios from 'axios';
 
 class Explore extends React.Component {
-  state = {
-    randomTrait: "",
-  };
+ 
+  constructor(props) {
+    super(props);
+    this.state = { names: [], descriptions: [], };
+
+     // this.handleClick = this.handleClick.bind(this);
+  }
+
+
+
+  componentDidMount(){
+    axios.get(`https://athena-back-end.herokuapp.com/api/allphilosophies` )
+      .then((response) => {
+          const names = [];
+          const descriptions = [];
+          const data = response.data
+
+          for (var i = 0 ; i < data.length; i++) {
+              names.push(data[i].philosophy);
+              descriptions.push(data[i].description);
+          }
+          this.setState({ names: names, descriptions: descriptions });
+          console.log(this.state);
+    });
+
+  }
+
+
+
 
   render() {
     return (	
       <div id="container">
         <h3>Explore Philosophies</h3>
+        {/* <button onClick={this.handleClick}>Get Philosophy Data</button> */}
         <div className = "philosophies">
           <div className = "philosophy-card">
             <div className = "philosophy-card-image">
@@ -23,17 +51,77 @@ class Explore extends React.Component {
               <Link to={{
                 pathname: '/philosophy',
                 aboutProps: {
-                  name: "Romanticism",
-                  description: "Lovely things",
-                  quote: "Please fall in love",
-                  quotee: "William the Shakespeare"
+                  name: this.state.names[0],
+                  description: this.state.descriptions[0],
+                  quote: "Taoist quote",
+                  quotee: "Taoist quotee"
                 }
-              }}><h4>Romanticism</h4></Link>
-              <p className="philosophy-tags">#Nature #Expression</p>
+              }}><h4>{this.state.names[0]}</h4></Link>
+              <p className="philosophy-tags"></p>
             </div>
           </div>
 
           <div className = "philosophy-card">
+            <div className = "philosophy-card-image">
+              <a href="#">
+                <img src = "https://via.placeholder.com/250" />
+              </a>
+            </div>
+            <div className = "philosophy-card-descr">
+              <Link to={{
+              pathname: '/philosophy',
+              aboutProps: {
+                name: this.state.names[1],
+                description: this.state.descriptions[1] ,
+                quote: "Stoic quote",
+                quotee: "Stoic quotee"
+              }
+            }}><h4>{this.state.names[1]}</h4></Link>
+              <p className="philosophy-tags"></p>
+            </div>
+          </div>
+
+          <div className = "philosophy-card">
+            <div className = "philosophy-card-image">
+              <a href="#">
+                <img src = "https://via.placeholder.com/250" />
+              </a>
+            </div>
+            <div className = "philosophy-card-descr">
+              <Link to={{
+              pathname: '/philosophy',
+              aboutProps: {
+                name: this.state.names[2],
+                description: this.state.descriptions[2],
+                quote: "Realist quote",
+                quotee: "Realist quotee"
+              }
+            }}><h4>{this.state.names[2]}</h4></Link>
+              <p className="philosophy-tags"></p>
+            </div>
+          </div>
+
+          <div className = "philosophy-card">
+            <div className = "philosophy-card-image">
+              <a href="#">
+                <img src = "https://via.placeholder.com/250" />
+              </a>
+            </div>
+            <div className = "philosophy-card-descr">
+              <Link to={{
+              pathname: '/philosophy',
+              aboutProps: {
+                name: this.state.names[3],
+                description: this.state.descriptions[3],
+                quote: "Actualist quote",
+                quotee: "Actualist quotee"
+              }
+            }}><h4>{this.state.names[3]}</h4></Link>
+              <p className="philosophy-tags"></p>
+            </div>
+          </div>
+
+          {/* <div className = "philosophy-card">
             <div className = "philosophy-card-image">
               <a href="#">
                 <img src = "https://via.placeholder.com/250" />
@@ -51,67 +139,7 @@ class Explore extends React.Component {
             }}><h4>Stoicism</h4></Link>
               <p className="philosophy-tags">#Nature #Expression</p>
             </div>
-          </div>
-
-          <div className = "philosophy-card">
-            <div className = "philosophy-card-image">
-              <a href="#">
-                <img src = "https://via.placeholder.com/250" />
-              </a>
-            </div>
-            <div className = "philosophy-card-descr">
-              <Link to={{
-              pathname: '/philosophy',
-              aboutProps: {
-                name: "Stoicism",
-                description: "Don't complain",
-                quote: "Be Strong",
-                quotee: "Marcus Aurelius"
-              }
-            }}><h4>Stoicism</h4></Link>
-              <p className="philosophy-tags">#Nature #Expression</p>
-            </div>
-          </div>
-
-          <div className = "philosophy-card">
-            <div className = "philosophy-card-image">
-              <a href="#">
-                <img src = "https://via.placeholder.com/250" />
-              </a>
-            </div>
-            <div className = "philosophy-card-descr">
-              <Link to={{
-              pathname: '/philosophy',
-              aboutProps: {
-                name: "Stoicism",
-                description: "Don't complain",
-                quote: "Be Strong",
-                quotee: "Marcus Aurelius"
-              }
-            }}><h4>Stoicism</h4></Link>
-              <p className="philosophy-tags">#Nature #Expression</p>
-            </div>
-          </div>
-
-          <div className = "philosophy-card">
-            <div className = "philosophy-card-image">
-              <a href="#">
-                <img src = "https://via.placeholder.com/250" />
-              </a>
-            </div>
-            <div className = "philosophy-card-descr">
-              <Link to={{
-              pathname: '/philosophy',
-              aboutProps: {
-                name: "Stoicism",
-                description: "Don't complain",
-                quote: "Be Strong",
-                quotee: "Marcus Aurelius"
-              }
-            }}><h4>Stoicism</h4></Link>
-              <p className="philosophy-tags">#Nature #Expression</p>
-            </div>
-          </div>          
+          </div>           */}
         
   
     
