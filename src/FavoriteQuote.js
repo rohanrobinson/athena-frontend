826c91a12c0e531b548d22b33903ee942e7d313e
ckrivickas <chasekrivickas@Chases-MBP.fios-router.home>
2021-03-07 11:17:56 -0500

    incorporated remove quote

--- src/FavoriteQuote.js
+++ src/FavoriteQuote.js
@@ -1,8 +1,12 @@
 import axios from "axios";
 import React, { useEffect, useState } from "react";
 import "./FavoriteQuote.css";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faTrash } from "@fortawesome/free-solid-svg-icons";
+import { useHistory } from "react-router-dom";
 
 const FavoriteQuote = ({ quoteId }) => {
+  const history = useHistory();
   const [quote, setQuote] = useState();
   const [author, setAuthor] = useState();
 
@@ -20,10 +24,49 @@ const FavoriteQuote = ({ quoteId }) => {
       });
   }, []);
 
+  const removeQuote = (event) => {
+    console.log("remove quote");
+    console.log(quoteId);
+    console.log("user:");
+    console.log(JSON.parse(sessionStorage.getItem('user'))._id.$oid);
+
+    // remove quote from liked
+    const config = {
+      headers: {
+        Authorization: 'Bearer ' + sessionStorage.getItem('token')
+      }
+    };
+    const body = {
+      removeQuote: quoteId
+    };
+    console.log(body);
+    axios.put(`https://athena-back-end.herokuapp.com/api/auth/removeQuote/${JSON.parse(sessionStorage.getItem('user'))._id.$oid}`, body, config)
+    .then((res) => {
+      // success, get new user object
+      console.log(res);
+      axios.get(`https://athena-back-end.herokuapp.com/api/auth/get/${JSON.parse(sessionStorage.getItem('user'))._id.$oid}`, config)
+        .then((response) => {
+          // success
+          console.log(response);
+          sessionStorage.setItem('user', JSON.stringify(response.data));
+          history.go(0);
+        })
+        .catch((error) => {
+          // error
+          console.log(error);
+        });
+    })
+    .catch((err) => {
+      // error
+      console.log(err);
+    });
+  }
+
   return (
     <div className="favQuoteCont">
         <h5>{quote}</h5>
         <p>{author}</p>
+        <FontAwesomeIcon icon={faTrash} className="trash_icon" onClick={removeQuote} />
     </div>
   )
 }
